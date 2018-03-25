a74c6cae44b739a74ce76cf4844feb450df15c9c
sa <kunaksergey@ukr.net>
2018-03-25 10:12:51 +0300

    add save

--- src/rcpapp/command/SaveNodeHandler.java
+++ src/rcpapp/command/SaveNodeHandler.java
@@ -3,12 +3,18 @@ package rcpapp.command;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
 
 public class SaveNodeHandler extends AbstractHandler{
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		// TODO Auto-generated method stub
+		IEditorPart editor=PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		if (editor.isDirty()){
+			System.out.println("Сохраняеем");
+		}
 		return null;
 	}
 
